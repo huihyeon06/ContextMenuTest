974814cdb712d7fe07350277be2cf7e0d40f2cfb
huihyeon06 <agnea0829@gmail.com>
2023-08-31 19:13:29 +0900

    실습 7-2(Context Menu) 완성

--- app/src/main/java/kr/hs/emirim/contextmenutest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/contextmenutest/MainActivity.kt
@@ -1,9 +1,11 @@
 package kr.hs.emirim.contextmenutest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
@@ -23,21 +25,52 @@ class MainActivity : AppCompatActivity() {
 
         registerForContextMenu(btn1)
         registerForContextMenu(btn2)
+    }
 
-        fun onCreateContextMenu(
-            menu: ContextMenu?,
-            v: View?,
-            menuInfo: ContextMenu.ContextMenuInfo?
-        ) {
-            super.onCreateContextMenu(menu, v, menuInfo)
-            var mInflater = this.menuInflater
-            if(v===btn1){
-                menu!!.setHeaderTitle("배경색 변경")
-                mInflater.inflate(R.menu.menu1, menu)
-            }
-            if(v===btn2){
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        var mInflater = this.menuInflater
+        if(v === btn1){
+            menu!!.setHeaderTitle("배경색 변경")
+            mInflater.inflate(R.menu.menu1, menu)
+        }
+            if (v === btn2) {
                 mInflater.inflate(R.menu.menu2, menu)
             }
         }
+
+        override fun onContextItemSelected(item: MenuItem): Boolean {
+            super.onContextItemSelected(item)
+            when(item.itemId){
+                R.id.itemRed->{
+                    linear.setBackgroundColor(Color.RED)
+                    return true
+                }
+                R.id.itemBlue->{
+                    linear.setBackgroundColor(Color.BLUE)
+                    return true
+                }
+                R.id.itemYellow->{
+                    linear.setBackgroundColor(Color.YELLOW)
+                    return true
+                }
+                R.id.subRotate->{
+                    btn1.rotation=180f
+                    return true
+                }
+                R.id.subSize->{
+                    btn1.scaleX=2.0f
+                    return true
+                }
+                R.id.subOriginal->{
+                    btn1.rotation=0f
+                    btn1.scaleX = 1f
+                    return true
+                }
+            }
+            return false
+        }
     }
-}
